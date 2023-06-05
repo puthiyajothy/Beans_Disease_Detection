a7eb416f2d70a4a074af8fa3ae10a53f2bd1a780
Mahendran Puthiyajothy <48283536+puthiyajothy@users.noreply.github.com>
2023-06-05 11:36:09 +0530

    first commit

---
+++ api/main.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+app = FastAPI()
+
+# Define the allowed origins
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+# Add the CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Load the TensorFlow model
+MODEL = tf.keras.models.load_model("D:/beans-Disease/models/1")
+CLASS_NAMES = ['angular_leaf_spot', 'bean_rust', 'healthy']
+
+@app.get("/ping")
+async def ping():
+    return "Hello, I am alive"
+
+def read_file_as_image(data) -> np.ndarray:
+    image = Image.open(BytesIO(data))
+    image = image.resize((256, 256))  # Resize the image to (256, 256)
+    image_array = np.array(image)
+    return image_array
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    
+    predictions = MODEL.predict(img_batch)
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)
